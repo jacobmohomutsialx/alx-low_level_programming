f650858f89318c75aa237c5d8643ed1da0cdae66
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-21 12:49:37 -0800

    using for loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+_putchar(l);
+_putchar('\n');
+}
