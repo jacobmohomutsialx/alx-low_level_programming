2ea7b68ccf0459bb98b76dec2b32ea5d189fc421
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-21 13:00:42 -0800

    nested for loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+void print_alphabet_x10(void)
+{
+char l;
+int x = 0;
+
+for (x = 0; x < 10; x++)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
