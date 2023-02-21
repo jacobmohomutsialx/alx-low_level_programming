d0d9d7f7372f96190f6e0f1ca17ad5b675e246fd
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-21 13:14:21 -0800

    funtion error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - checks if input is lowercase
- * @c: variable to determine if input is lowercase
+ * _islower - checks if input is lowercase or uppercase
+ * @c: variable to determine if input is lowercase or uppercase
  * main - uses built in function
  * Return: 0 if succesful
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 int i;
 i = 0;
