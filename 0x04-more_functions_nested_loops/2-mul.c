b7f138a424871faa5101d864a4ce00de310470d0
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-23 07:21:06 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - checks if input is a digit
+ * @a: is the value parsed
+ * @b: is the value parsed
+ * Return: 1
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
