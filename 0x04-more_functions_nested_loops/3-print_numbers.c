943772f98e7f421d1a99ba9f6192615a3940ff90
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-23 07:37:13 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks if input is a digit
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+for (int i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+}
