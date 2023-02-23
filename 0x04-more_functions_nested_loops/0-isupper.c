a715aea33bd85442e1d044130c977968b56a7a1a
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-23 06:42:01 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - checks if character is uppercase
+ * @c takes in the input character
+ * Return: 1 If character is uppercase
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+}
