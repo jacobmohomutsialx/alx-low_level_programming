3fb882c2e5e2354067061a39b2cccbd42e4fb83d
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-23 11:24:11 -0800

    forgot semicolon on prototype

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks if input is a digit
- *
- *
+ * print_most_numbers - prints numbers 0 too 9 except for 2&4 then newline
  */
 
 void print_most_numbers(void)
@@ -12,7 +10,9 @@ int i;
 for (i = 0; i < 10; i++)
 {
 if (i == 2 || i == 4)
+{
 continue;
+}
 _putchar(i + '0');
 }
 _putchar('\n');
