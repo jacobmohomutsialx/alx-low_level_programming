f64c44944fc0255c9192505f62215c42017f918a
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-02-27 13:55:22 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value it points to
+ * @n: variable to be updated
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+
+return (*n);
+}
