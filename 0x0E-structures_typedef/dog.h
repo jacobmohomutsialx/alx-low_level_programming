71cf506ffe2dd556f2d4adfe93b7cdcb610eb4ee
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-03-20 14:20:21 -0700

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - The dog struct
  * @name: First member
@@ -6,9 +9,14 @@
  *
  * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /*DOG_H*/
